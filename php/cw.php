e74c13f1442674c5b2b087713952ed284bc338d9
sc <toothpik6@gmail.com>
2015-06-03 20:46:51 -0500

    just want the first error

--- php/cw.php
+++ php/cw.php
@@ -1,12 +1,8 @@
 <?php
-$in = fopen("http://forecast.weather.gov/MapClick.php?lat=39.16954867942911&lon=-94.52716875183569&site=all&smap=1", "r");
 #$in = fopen("http://forecast.weather.gov/MapClick.php?lat=39.160078&lon=-94.53150289999996&site=all&smap=1&searchresult=Kansas%20City%2C%20MO%2064117%2C%20USA#.UooxIcO0XmF", "r");
-#$in = fopen("http://forecast.weather.gov/MapClick.php?lat=39.16954867942911&lon=-94.52716875183569&site=all&smap=1", "r");
-#$in = fopen("http://forecast.weather.gov/MapClick.php?lat=39.160078&lon=-94.53150289999996&site=all&smap=1&searchresult=Kansas%20City%2C%20MO%2064117%2C%20USA#.Utbf15W0VQK", "r");
-#$in = fopen("http://forecast.weather.gov/MapClick.php?lat=39.1854331703021&lon=-94.55859392881393#.UstIypW0VQI", "r");
-#$in = fopen("http://forecast.weather.gov/MapClick.php?zoneid=MOZ029&zflg=1", "r");
-while (!feof($in)) {
-    $buffer = fread($in, 2048);
-    print($buffer);
-}
+$in = fopen("http://forecast.weather.gov/MapClick.php?CityName=Kansas+City&state=MO&site=EAX&lat=39.0904&lon=-94.5837", "r");
+#while (!feof($in)) {
+#    $buffer = fread($in, 2048);
+#    print($buffer);
+#}
 ?>
