6d0bfb3973adcac1e17e7c1b449c0bf07d79314d
tooth pik <toothpik6@gmail.com>
2018-07-25 15:57:49 -0500

    first addition

--- php/cw.php
+++
@@ -1,8 +0,0 @@
-<?php
-#$in = fopen("http://forecast.weather.gov/MapClick.php?lat=39.160078&lon=-94.53150289999996&site=all&smap=1&searchresult=Kansas%20City%2C%20MO%2064117%2C%20USA#.UooxIcO0XmF", "r");
-$in = fopen("http://forecast.weather.gov/MapClick.php?CityName=Kansas+City&state=MO&site=EAX&lat=39.0904&lon=-94.5837", "r");
-#while (!feof($in)) {
-#    $buffer = fread($in, 2048);
-#    print($buffer);
-#}
-?>
